30079ed1b0503949274c5a6ae12b1bea36b821ab
= <=>
2023-04-24 06:19:21 -0600

    Added final backend code (server)

--- House Price Prediction/Code/Server/server.py
+++ House Price Prediction/Code/Server/server.py
@@ -24,8 +24,27 @@ def get_location_names():
     response.headers.add('Access-control-Allow-Origin','*')
     return response
 
+@app.route('/predict_home_price', methods = ['POST'])
+
+def predict_home_price():
+    
+    total_sqft = float(request.form['total_sqft'])
+    location = request.form['location']
+    bhk = int(request.form['bhk'])
+    bath = int(request.form['bath'])
+    
+    try:
+        response = jsonify({
+                'estimated_price' : util.get_estimated_price(location, total_sqft, bhk, bath)
+                })
+    except Exception as err:
+        print(err)
+    
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 if __name__ == "__main__":
     print("Starting Python Flask Server for Home Price Prediction")
+    util.load_saved_artifacts()
     app.run()
     
--- House Price Prediction/Code/Server/util.py
+++ House Price Prediction/Code/Server/util.py
@@ -6,12 +6,31 @@ Created on Sat Apr 22 06:58:11 2023
 """
 import json
 import pickle
+import numpy as np
 
 __locations = None
 __data_columns = None
 __model = None
 
-def get_estimated_price():
+def get_estimated_price(location, sqft, bhk, bath):
+    # with open("./artifacts/banglore_home_prices_model.pickle", "rb") as f:
+    #     __model = pickle.load(f)
+        
+    try:
+        loc_index = __data_columns.index(location.lower())
+    except:
+        loc_index = -1
+    
+    x = np.zeros(len(__data_columns))
+    x[0] = sqft
+    x[1] = bath
+    x[2] = bhk
+    
+    if loc_index >= 0:
+        x[loc_index] = 1   
+    
+    return round(__model.predict([x])[0],2)
+
     
 
 def get_location_names():
@@ -25,6 +44,7 @@ def load_saved_artifacts():
     print("loding saved artifacts... start")
     global __data_columns
     global __locations
+    global __model
     
     with open("./artifacts/columns.json","r") as f:
         __data_columns = json.load(f)['data_columns']
@@ -39,3 +59,7 @@ def load_saved_artifacts():
 if __name__ == '__main__':
     load_saved_artifacts()
     print(get_location_names())
+    print(get_estimated_price('1st Phase P Nagar', 1000, 3, 3))
+    print(get_estimated_price('1st Phase P Nagar', 1000, 2, 2))
+    print(get_estimated_price('Ejipura', 1000, 2, 2))
+    print(get_estimated_price('Kalhalli', 1000, 2, 2))
